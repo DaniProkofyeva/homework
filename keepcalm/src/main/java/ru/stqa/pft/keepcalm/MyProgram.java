98b150b3d1e88d0bb160f782f52afb6c44db5d44
DaniProkofyeva <daria.dm.leonova@gmail.com>
2020-02-09 19:53:52 +0300

    Изменены настройки форматирования

--- keepcalm/src/main/java/ru/stqa/pft/keepcalm/MyProgram.java
+++ keepcalm/src/main/java/ru/stqa/pft/keepcalm/MyProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.keepcalm;
 
 public class MyProgram {
 
-  public static void main(String[] args) {
-System.out.println("Hi All");
+    public static void main(String[] args) {
+        System.out.println("Hi All");
 
-}
+    }
 }

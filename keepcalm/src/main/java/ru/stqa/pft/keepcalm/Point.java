5a177e8ece40cd03d6e5c095707f2f6d59ad19bd
DaniProkofyeva <daria.dm.leonova@gmail.com>
2020-02-15 15:21:19 +0300

    Задание 2. Попытка 2.

--- keepcalm/src/main/java/ru/stqa/pft/keepcalm/Point.java
+++ keepcalm/src/main/java/ru/stqa/pft/keepcalm/Point.java
@@ -1,14 +1,20 @@
 package ru.stqa.pft.keepcalm;
 
 public class Point {
-  public double x1;
-  public double x2;
-  public double y1;
-  public double y2;
 
+  public double x;
+  public double y;
 
-  public double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1) * (p2.y2 - p1.y1));
+  public Point(double x, double y){
+
+    this.x = x;
+    this.y = y;
+
   }
 
+  public double distance(Point p1){
+
+    return Math.sqrt((this.x - p1.x) * (this.x - p1.x) + (this.y - p1.y) * (this.y - p1.y));
+
+  }
 }
--- keepcalm/src/main/java/ru/stqa/pft/keepcalm/PointClass.java
+++ keepcalm/src/main/java/ru/stqa/pft/keepcalm/PointClass.java
@@ -2,22 +2,13 @@ package ru.stqa.pft.keepcalm;
 
 public class PointClass{
 
-    public static double x1;
-    public static double x2;
-    public static double y1;
-    public static double y2;
+    public static void main(String[] args) {
 
-    public static void main (String[] args) {
+      Point p1 = new Point(13, 4);
+      Point p2 = new Point(-3, 7);
 
-      Point p1 = new Point();
-      Point p2 = new Point();
+      System.out.println("Расстояние между точками " + p1.distance(p2));
 
-      p1.x1 = 13;
-      p2.x2 = -3;
-      p1.y1 = 4;
-      p2.y2 = 7;
-
-      System.out.println("Расстояние между точками " + p1.distance(p1,p2));
     }
 
   }

067945c0f3ae0a60670715c3b1153c0bf591aa19
DaniProkofyeva <daria.dm.leonova@gmail.com>
2020-04-08 19:38:56 +0300

    REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stga/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stga.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("77e3fd0e9f2ef987014dc05ceb51bcce842e880c");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("DaniProkofyeva", "homework")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
